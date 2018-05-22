27c1c18ef9f85e9ad618ee909baa2d746db1fbc3
TooAndy <aniss@qq.com>
2018-05-22 09:24:15 +0800

    change concurrent_requests = 10 && download delay = 3.5

--- apks/settings.py
+++ apks/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = 'apks.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 10
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 3.5
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -71,6 +71,9 @@ ITEM_PIPELINES = {
     # 'scrapy.pipelines.files.FilesPipeline': 1
     # 'apks.pipelines.CustomPipeline': 2,
 }
+
+RETRY_HTTP_CODES = [500, 502, 503, 504, 522, 524, 408, 403, 404, 400]
+RETRY_TIMES = 5
 
 # FILES_STORE = "download_apks"  # download directory
 # DOWNLOAD_MAXSIZE = 10000000000  # MaxSize of file
